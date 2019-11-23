c8d21da6af4a5acbe2e7b27f3d358fbef2d9d97d
Alessia Trianti <alestrianti@gmail.com>
2019-11-23 16:18:23 +0100

    Added routes

--- post-app/app.js
+++ post-app/app.js
@@ -1,12 +1,43 @@
+// Require dependencies
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
+// Require modules
+const Post = require('./models/post');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
+
+// Routes
+app.get('/', (req, res) => {
+    Post.find({}, (err, posts) => {
+        if (err) { 
+            console.log(err);
+            res.render('index');
+        } else {
+            res.render('index', { posts: posts });
+        }
+    });
+});
+
+app.post('/posts', (req, res) => {
+    try {
+        const newPost = new Post({
+            title: req.body.title,
+            content: req.body.content
+        });
+    
+        newPost.save();
+    } catch(err) {
+        throw err;
+    }
+
+    res.redirect('/');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
